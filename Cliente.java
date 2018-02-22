6222256f2b405aa858dad98df445c60d7589967b
MarchCastro <march.castrof@gmail.com>
2018-02-21 20:09:21 -0600

    Servidor

---
+++ Cliente.java
@@ -0,0 +1,96 @@
+import java.net.*;
+import java.io.*;
+import javax.swing.JFileChooser; //Caja de dialogo donde selecciono el archivo que quiero
+import javax.swing.plaf.FileChooserUI;
+//Investigr como implementar la funcionalidad Drag & Drop
+public class Cliente{
+    static JFileChooser jf;
+
+    /*public static void Envia_datos(File f, Socket cl){
+        try{
+            //DataOutputStream dos = null;
+            //DataInputStream dis = null;
+
+            int porcentaje,n = 0;
+            String path = f.getAbsolutePath();
+            String nombre = f.getName();
+            long tam = f.length();
+
+            System.out.println("Conexion establecida ... iniciando envío del archivo" + nombre + "\n\n");
+            
+            long enviados = 0;
+            byte [] b = new byte [1500];
+            DataOutputStream dos = new DataOutputStream(cl.getOutputStream());
+            DataInputStream dis = new DataInputStream(new FileInputStream(path));
+            dos.writeUTF(nombre);
+            dos.flush();
+            dos.writeLong(tam);
+            dos.flush();
+
+            while(enviados < tam){
+                n = dis.read(b);
+                dos.write(b,0,n);
+                enviados += n;
+                porcentaje = (int)((enviados * 100)/tam);
+                System.out.println("\r Se ha enviado el " + porcentaje + "% del archivo");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }*/
+
+    public static void main(String [] args){
+        try{
+            int pto = 9000;
+            BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("\n Escribe la dirección del servidor: ");
+            String dir = br1.readLine();
+            System.out.print("\n Escribe la dirección de la carpeta: ");
+            String path = br1.readLine();
+
+            Socket cl = new Socket(dir, pto);
+            //Envio opcion elegida por usuario
+            PrintWriter opc = new PrintWriter(new OutputStreamWriter(cl.getOutputStream()));
+            opc.println(path);
+            opc.flush();
+
+            //Recibo numero de archivos
+            BufferedReader br3 = new BufferedReader(new InputStreamReader(cl.getInputStream()));
+            String numero = "";
+            numero = br3.readLine();
+            int numero1 = Integer.parseInt(numero);
+            System.out.println("\n Hay: " + numero1);
+
+            for(int i = 0; i < numero1; i++){
+                DataInputStream dis = new DataInputStream(cl.getInputStream());
+                String nombre = dis.readUTF();
+                System.out.println("Inicia recepción del archivo: " + nombre );
+            }
+            
+            /*
+            System.out.print("\n Estableciendo la conexión con el servidor");
+            System.out.print("\n Conexión establecida");
+
+            jf = new JFileChooser();
+            jf.setMultiSelectionEnabled(true);
+            
+            int r = jf.showOpenDialog(null);
+            int n = 0;
+            Socket cl = null;
+            if(n == JFileChooser.APPROVE_OPTION){
+                
+                File [] f = jf.getSelectedFiles();
+                System.out.println("\n 1 "+ f[0]);
+                System.out.println("\n 2 "+ f[1]);
+                for(int i = 0; i < f.length; i++){
+                    cl = new Socket(dst,pto);
+                    //Envia_datos(f[i], cl);
+                }
+                System.out.println("\n Se ha completado el envio");
+                cl.close();
+            }*/
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Servidor.java
@@ -0,0 +1,73 @@
+import java.net.*;
+import java.io.*;
+
+public class Servidor{
+    public static void main(String[] args) {
+
+        try{
+            ServerSocket s = new ServerSocket(9000);
+            s.setReuseAddress(true);
+            System.out.println("Servicio iniciado, esperando clientes");
+            
+            //Conexion con cliente
+            Socket cl = s.accept();
+            System.out.println("Cliente conectado desde:"+cl.getInetAddress()+":"+cl.getPort());
+                
+            BufferedReader br3 = new BufferedReader(new InputStreamReader(cl.getInputStream()));
+            String path = "";
+            path = br3.readLine();
+            System.out.println("\n La direccion es: " + path);
+           
+            File f = new File(path);
+            System.out.println("\n File " + f);
+            if(f.exists()){ 
+                File[] carpetas = f.listFiles();
+                //Envio numero de archivos
+                PrintWriter numero = new PrintWriter(new OutputStreamWriter(cl.getOutputStream()));
+                numero.println(carpetas.length);
+                numero.flush();
+                //Envio nombres
+                for (int x = 0; x < carpetas.length; x++){
+                    System.out.println(carpetas[x].getName());
+                    DataOutputStream dos = new DataOutputStream(cl.getOutputStream());
+                    dos.writeUTF(carpetas[x].getName());
+                    dos.flush();
+                   
+                }
+            }else{ 
+                System.out.println("No existe la carpeta :(");
+            }
+            
+            
+            
+            /*
+            for(;;){
+                Socket cl = s.accept();
+                DataIputStream dis = new DataInputStream(cl.getInputStream());
+                String nombre = dis.readUTF();
+                long tam = dis.readLong();
+                System.out.println("Inicia recepción del archivo: " + nombre + " de tamaño " + tam + " desde " + cl.getInetAddress() + ":" + cl.getPort());
+                DataOutputStream dos = new DataOutputStream(new FileOutputStream(nombre));
+                long recibidos = 0;
+                int n,porcentaje = 0;
+                
+                while(recibidos < tam){
+                    byte [] b = new byte[1500];
+                    n = dis.read(b);
+                    recibidos += n;
+                    dos.write(b,0,n);
+                    dos.flush();
+                    porcentaje = (int)((recibidos * 100)/tam);
+                    System.out.println("\r Recibido el " + porcentaje + "% del archivo");
+                }
+
+                System.out.println("Archivo recibido");
+                dos.close();
+                dis.close();
+                cl.close();
+            }*/
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
